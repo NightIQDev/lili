2e5a3df878a0303b655e571bcb874c03da0c1b07
juanandresojeda77@gmail.com <juanandresojeda77@gmail.com>
2023-08-22 02:42:00 -0500

    myfood v.0.0.1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import 'https://kit.fontawesome.com/120031ba31.js'
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <div className="text-center">
+        <h1 className="myfont text-9xl ">MyFood</h1>
+        <h1 className="myfont text-5xl animate-pulse">Proximamente</h1>
+      </div>
+
+      <div className="mx-auto flex flex-col justify-around w-screen my-4">
+        <div className="mx-auto flex justify-around w-32 my-4">
+          <a href="https://www.instagram.com/oficialmyfood"><i class="fa-brands fa-instagram text-4xl icons-colors"></i></a>
+          <a href="https://www.tiktok.com/@oficialmyfood" ><i class="fa-brands fa-tiktok text-4xl icons-colors"></i></a>
+        </div>
+      </div >
+    </div >
+  );
+}
+
+export default App;
